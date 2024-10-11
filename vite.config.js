80e70af231ae5228368e57fcb52241eefc3d86a0
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2024-10-11 19:58:55 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-      plugins: [react(), svgr()],
-      resolve: {
-        alias: {
-          src: '/src',
-          components: '/src/components',
-          pages: '/src/pages',
-          assets: '/src/assets'
-        },
-      },
-      base: '/react-vite-test/',
-    });
+  plugins: [react()],
+})
